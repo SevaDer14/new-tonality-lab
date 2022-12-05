417720b07700d821c65f2510e5f220f5d48cbfc1
Seva Deriushkin <vsevolod.deriushkin@gmail.com>
2022-12-05 08:17:20 +0100

    adds vercel audiencies

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,6 @@
+import { inject } from '@vercel/analytics';
+
+inject();
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
